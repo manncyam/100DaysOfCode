6a7b3c8e0d65436a25627d46c4bbbb564fba4ea8
manncyam <manncyam@gmail.com>
2023-08-24 05:32:02 -0700

    Added Day1 Project

---
+++ Day1/main.py
@@ -0,0 +1,8 @@
+def main():
+    print("Welcome to the Band Name Generator.")
+    city = input("What's name of the city you grew up in?\n")
+    pet_name = input("What's your pet's name?\n")
+    print("Your band name could be " + city + " " + pet_name)
+    
+if __name__ == "__main__":
+    main()
