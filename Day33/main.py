d0ca93808012fea873cb073665265b941965937b
manncyam <manncyam@gmail.com>
2023-09-29 05:39:17 -0700

    add iss location tracking

---
+++ Day33/main.py
@@ -0,0 +1,39 @@
+import requests
+from datetime import datetime
+import time
+
+MY_LAT = 33.745472
+MY_LONG = -117.867653
+
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    #Your position is within +5 or -5 degrees of the ISS position.
+    return abs(MY_LAT - iss_latitude) < 5 and abs(MY_LONG - iss_longitude)
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+
+    return time_now >= sunset or time_now <= sunrise
+
+while True:
+    if is_iss_overhead() and is_night():
+        print("send an email to tell me to look up.")
+    time.sleep(60)
